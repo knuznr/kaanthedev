def22105144d03df341979edd4bb004892f1ff26
knnuznr <70298812+knnuznr@users.noreply.github.com>
2024-07-22 23:37:32 +0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,21 @@
-import { BlogPosts } from 'app/components/posts'
+import { BlogPosts } from "app/components/posts";
 
 export default function Page() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+      <h1 class="mb-8 text-2xl font-medium tracking-tighter">
+        hey, I'm kaan! 👋
       </h1>
-      <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+      <p className="prose prose-neutral dark:prose-invert">
+        {`I am a software developer with a deep passion for coding, coffee, and technology. My journey in the tech world has been fueled by a relentless curiosity and a love for problem-solving. Whether it’s writing clean, efficient code or exploring the latest advancements in technology, I am always eager to learn and innovate.
+
+When I’m not immersed in coding, you’ll find me enjoying a good cup of coffee, which fuels my creativity and productivity. My passion for technology goes beyond just writing code; I am enthusiastic about understanding how things work and finding ways to make them better.
+
+With a keen eye for detail and a commitment to excellence, I strive to create software solutions that are not only functional but also user-friendly. I am excited about the endless possibilities that technology offers and look forward to contributing to projects that make a difference.`}
       </p>
-      <div className="my-8">
+      <div className="my-8 flex w-full flex-col space-y-4">
         <BlogPosts />
       </div>
     </section>
-  )
+  );
 }
