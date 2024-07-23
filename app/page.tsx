071c643686b74e6b42d6320b944c24a3d3938e8a
kaanuznr <kaanuznr@gmail.com>
2024-07-23 13:34:46 +0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,46 @@
 import { BlogPosts } from "app/components/posts";
 import Head from "next/head";
+import { Suspense } from 'react';
+
 import Link from 'next/link';
 
+function ArrowIcon() {
+  return (
+    <svg
+      width="12"
+      height="12"
+      viewBox="0 0 12 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
 
+function BlogLink({ slug, name }) {
+  return (
+    <div className="group">
+      <a
+        href={`/blog/${slug}`}
+        className="flex w-full items-center justify-between rounded border border-neutral-200 bg-neutral-50 px-3 py-4 dark:border-neutral-700 dark:bg-neutral-800"
+      >
+        <div className="flex flex-col">
+          <p className="font-medium text-neutral-900 dark:text-neutral-100">
+            {name}
+          </p>
+          
+        </div>
+        <div className="transform text-neutral-700 transition-transform duration-300 group-hover:-rotate-12 dark:text-neutral-300">
+          <ArrowIcon />
+        </div>
+      </a>
+    </div>
+  );
+}
 export default function Page() {
   return (
     <section>
@@ -25,7 +63,8 @@ export default function Page() {
         {` me!`}
       </p>
       <div className="my-8 flex w-full flex-col space-y-4">
-        <BlogPosts />
+        <BlogLink name="Explanation of 'Time Complexity'" slug="time-complexity" />
+        <BlogLink name="Types of Sorting Algorithms" slug="sorting-algorithms" />
       </div>
     </section>
   );
