f4251139c30dc53fe93e84fdbc0f6f6af6736179
kaanuznr <kaanuznr@gmail.com>
2024-12-11 23:35:09 +0300

    changed background color

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,7 +50,7 @@ export default function RootLayout({
     <html
       lang="en"
       className={cx(
-        'text-black bg-white dark:text-white dark:bg-black',
+        'text-black bg-white dark:text-white dark:bg-[#181C14]',
         GeistSans.variable,
         GeistMono.variable
       )}
